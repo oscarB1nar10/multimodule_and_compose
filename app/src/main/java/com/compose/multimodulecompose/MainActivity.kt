2b0d91f7d162c30d9e151b32e87500eaa0e9251a
Oscar <oscivaramtin@gmail.com>
2021-09-21 22:22:09 -0500

    AS-10
    AS-10 (Testing GetHeros Use-case): Manual testing for GetHeros use-case was performed on App module (MainActivity) importing the required modules (core, herDomain and heroInteractors) to get the data from network in this case.

--- app/src/main/java/com/compose/multimodulecompose/MainActivity.kt
+++ app/src/main/java/com/compose/multimodulecompose/MainActivity.kt
@@ -3,37 +3,85 @@ package com.compose.multimodulecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.compose.core.DataState
+import com.compose.core.Logger
+import com.compose.core.ProgressBarState
+import com.compose.core.UIComponent
+import com.compose.hero_domain.Hero
+import com.compose.hero_interactors.HeroInteractors
 import com.compose.multimodulecompose.ui.theme.DotaInfoTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 
 class MainActivity : ComponentActivity() {
+
+    private val heros: MutableState<List<Hero>> = mutableStateOf(listOf())
+    private val progressBarState: MutableState<ProgressBarState> =
+        mutableStateOf(ProgressBarState.Idle)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val getHeros = HeroInteractors.build().getHeros
+        val logger = Logger("GetHerosTest")
+        getHeros.execute().onEach { dataState ->
+            when (dataState) {
+                is DataState.Response -> {
+                    when (dataState.uiComponent) {
+                        is UIComponent.Dialog -> {
+                            logger.log((dataState.uiComponent as UIComponent.Dialog).description)
+                        }
+
+                        is UIComponent.None -> {
+                            logger.log((dataState.uiComponent as UIComponent.Dialog).description)
+                        }
+                    }
+                }
+
+                is DataState.Data -> {
+                    heros.value = dataState.data ?: listOf()
+                }
+
+                is DataState.Loading -> {
+                    progressBarState.value = dataState.progressBarState
+                }
+            }
+        }.launchIn(CoroutineScope(IO))
+
         setContent {
             DotaInfoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Box(modifier = Modifier.fillMaxSize()) {
+                        LazyColumn {
+                            items(heros.value) { hero ->
+                                Text(text = hero.localizedName)
+                            }
+                        }
+
+                        if (progressBarState.value is ProgressBarState.Loading) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DotaInfoTheme {
-        Greeting("Android")
     }
 }
