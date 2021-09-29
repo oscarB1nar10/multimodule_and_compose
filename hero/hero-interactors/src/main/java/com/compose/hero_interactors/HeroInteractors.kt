536b29aef6456ae34bf80caaa0d3469f8e2a236c
Oscar <oscivaramtin@gmail.com>
2021-09-28 22:12:41 -0500

    AS-13
    AS-13 (Add Caching to GetHeros Use-case): Hero cache added to use-case to get heros, cache data is the single source of thrut.

--- hero/hero-interactors/src/main/java/com/compose/hero_interactors/HeroInteractors.kt
+++ hero/hero-interactors/src/main/java/com/compose/hero_interactors/HeroInteractors.kt
@@ -1,6 +1,8 @@
 package com.compose.hero_interactors
 
+import com.compose.hero_datasource.cache.HeroCache
 import com.compose.hero_datasource.network.HeroServices
+import com.squareup.sqldelight.db.SqlDriver
 
 data class HeroInteractors(
     val getHeros: GetHeros,
@@ -8,11 +10,12 @@ data class HeroInteractors(
 ) {
 
     companion object Factory {
-        fun build(): HeroInteractors {
+        fun build(sqlDriver: SqlDriver): HeroInteractors {
             val service = HeroServices.build()
+            val cache = HeroCache.build(sqlDriver = sqlDriver)
 
             return HeroInteractors(
-                getHeros = GetHeros(service = service)
+                getHeros = GetHeros(service = service, cache = cache)
             )
         }
     }
