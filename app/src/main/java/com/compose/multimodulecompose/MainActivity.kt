0c64585bd2b0e1f21022058497b233f284809329
Oscar <oscivaramtin@gmail.com>
2021-10-19 21:53:16 -0500

    AS-19

--- app/src/main/java/com/compose/multimodulecompose/MainActivity.kt
+++ app/src/main/java/com/compose/multimodulecompose/MainActivity.kt
@@ -9,21 +9,17 @@ import com.compose.multimodulecompose.ui.theme.DotaInfoTheme
 import com.compose.ui_hero_list.ui.HeroList
 import com.compose.ui_hero_list.ui.HeroListViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val imageLoader = ImageLoader.Builder(applicationContext)
-            .error(R.drawable.error_image)
-            .placeholder(R.drawable.white_background)
-            .availableMemoryPercentage(.25)
-            .crossfade(true)
-            .build()
-
 
         setContent {
             DotaInfoTheme {
